98117e9b9a98ef405490d7e7c353f71b2d3840af
tanvir <anjumtanvir667@gmail.com>
2025-01-24 04:29:11 +0600

    update allowed hosts

--- brainy_bean/settings.py
+++ brainy_bean/settings.py
@@ -35,7 +35,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1", "brainybean.up.railway.app"]
+
+CSRF_TRUSTED_ORIGINS = ["https://brainybean.up.railway.app"]
 
 
 # Application definition
