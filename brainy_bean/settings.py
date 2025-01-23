aeb62e8bad59624b510df8cb9336d65c05420b4a
tanvir <anjumtanvir667@gmail.com>
2025-01-24 04:19:05 +0600

    collect static output

--- brainy_bean/settings.py
+++ brainy_bean/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/' #url
-# STATIC_URL= BASE_DIR / 'staticfiles' #production
+STATIC_ROOT= BASE_DIR / 'staticfiles' #production
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static' #folder location
