37e425204f73d095af9c1b5f3251bfad8773d91b
tanvir <anjumtanvir667@gmail.com>
2025-01-29 01:52:21 +0600

    Revert "Project Updated 2"
    This reverts commit 69e0cdb34d340b8a508a4039865522ea99b58a3c.

--- brainy_bean/settings.py
+++ brainy_bean/settings.py
@@ -10,32 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-import environ
-import os
-
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-ydqio$b^+9$9gr&ni!pzg&5h=ar#!1r7n$bdxv_1dq4x5ssoq6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -60,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whiteNoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'brainy_bean.urls'
@@ -87,21 +76,10 @@ WSGI_APPLICATION = 'brainy_bean.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-        'NAME': os.environ["PGDATABASE"], 
-        'USER': os.environ["PGUSER"], 
-        'PASSWORD': os.environ["PGPASSWORD"], 
-        'HOST': os.environ["PGHOST"], 
-        'PORT': os.environ["PGPORT"],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
